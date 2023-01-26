12fb42f59570052a1c70b799c8cd1667074a9eac
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2023-01-26 20:45:50 +0000

    dont use dict, use list

--- 2021/6/main.py
+++ 2021/6/main.py
@@ -12,25 +12,22 @@ def part1(fish):
   print('fish', len(fish))
 
 def part2(fish):
-  day_fishnum_map = {}
-  for i in range(9):
-    day_fishnum_map[i] = 0
+  fish_list = [0 for i in range(9)]
   for f in fish:
-    day_fishnum_map[f] += 1
+    fish_list[f] += 1
   NUM_DAYS = 256
   for day in range(NUM_DAYS):
-    next_dict = {}
-    num_parents = day_fishnum_map[0] # day 0 fish will give birth
+    next_fish = [0 for i in range(9)]
+    parents = fish_list[0] # day 0 fish will give birth
     for i in range(9):
       if i == 0:
-        next_dict[8] = num_parents # spawn new fish
+        next_fish[8] = parents # spawn new fish
       else:
-        next_dict[i - 1] = day_fishnum_map[i]
-    next_dict[6] += num_parents # add parents back to day 6
-    day_fishnum_map = next_dict
-  print(day_fishnum_map)
-  fcount = sum([v for v in day_fishnum_map.values()])
-  print('fish', fcount)
+        next_fish[i - 1] = fish_list[i]
+    next_fish[6] += parents # add parents back to day 6
+    fish_list = next_fish
+  print(fish_list)
+  print('fish', sum(fish_list))
 
 
 if __name__ == '__main__':
