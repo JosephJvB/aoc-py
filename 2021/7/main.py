fab42d9bdfaaa7626fc7b375b71e13e8f79f1441
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2023-01-29 21:23:35 +0000

    7

---
+++ 2021/7/main.py
@@ -0,0 +1,45 @@
+memory = {}
+def calc_cost(n):
+  extra = 0
+  cost = 0
+  if memory.get(n):
+    return memory[n]
+  for x in range(n):
+    cost += 1
+    cost += extra
+    extra += 1
+  memory[n] = cost
+  return cost
+def part1():
+  crabs = [int(s) for s in open('input.txt').read().split(',')]
+  minx = min(crabs)
+  maxx = max(crabs)
+  print('min, max', minx, maxx)
+  cheapest = -1
+  xpos = -1
+  for x in range(minx, maxx):
+    cost = sum([abs(c - x) for c in crabs])
+    if cost < cheapest or cheapest == -1:
+      cheapest = cost
+      xpos = x
+  print('cheapest x pos', cheapest, xpos)
+
+# part1()
+def part2():
+  crabs = [int(s) for s in open('input.txt').read().split(',')]
+  print('num crabs', len(crabs))
+  minx = min(crabs)
+  maxx = max(crabs)
+  print('min, max', minx, maxx)
+  cheapest = -1
+  xpos = -1
+  for x in range(minx, maxx):
+    cost = sum([
+      calc_cost(abs(c - x)) for c in crabs
+    ])
+    if cost < cheapest or cheapest == -1:
+      cheapest = cost
+      xpos = x
+  print('cheapest x pos', cheapest, xpos)
+
+part2()
